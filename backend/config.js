6e8e6586727477a309f52ca7b65b025160473b98
abhii-007-code <tiwaryabhinav109@gmail.com>
2024-07-28 11:19:49 +0530

    final project

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://tiwaryabhinav109:FBa0uHov077BiVZf@cluster0.zmzzylc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster'
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
